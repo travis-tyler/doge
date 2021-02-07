f44e4b2dc7922ea5ebf1dac0b707ae7a2e87bdd3
travis-tyler <travisatyler@gmail.com>
2021-02-07 13:36:27 -0600

    Added trailing stop loss function

--- code/app.py
+++ code/app.py
@@ -8,14 +8,10 @@ import robin_stocks as rh
 import tweepy as t
 import time
 import config
+import funcs
 
-# Set up tweepy
-auth = t.OAuthHandler(config.api_key, config.secret_key)
-auth.set_access_token(config.token, config.token_secret)
-api = t.API(auth,wait_on_rate_limit=True)
-
-#Filters out mentions and retweets
 def from_creator(status):
+    '''Filters out mentions and retweets'''
     if hasattr(status, 'retweeted_status'):
         return False
     elif status.in_reply_to_status_id != None:
@@ -27,61 +23,43 @@ def from_creator(status):
     else:
         return True
 
+def stop_loss():
+    '''Sells stock when it drops 5% or more from its lastest high'''
+    buy_price = high_price = rh.get_crypto_quote('DOGE')['ask_price']
+    up = True
+    while up:
+        current_price = rh.get_crypto_quote('DOGE')['ask_price']
+        if current_price < high_price:
+            loss_percentage = (current_price*100)/high_price
+            if loss_percentage <= 95:
+                sell_amount = (20*current_price)/buy_price
+                rh.order_sell_crypto_by_price('DOGE', sell_amount)
+                up = False
+            else:
+                time.sleep(60)
+        else:
+            time.sleep(60)
+
+# Set up robin_stocks
+rh.login(config.user, config.pw)
+
+# Set up tweepy
+auth = t.OAuthHandler(config.api_key, config.secret_key)
+auth.set_access_token(config.token, config.token_secret)
+api = t.API(auth,wait_on_rate_limit=True)
+
 class MyStreamListener(t.StreamListener):
 
     def on_status(self, status):
         if from_creator(status):
             tweet = status.text.lower()
             if "doge" in tweet:
-
-                # Set number of day trades
-                # TODO: needs to be for week, not all time
-                # TODO: default to buying DOGE if last move was buying and vice versa
-                day_trades = len(rh.get_day_trades())
-
-                # Right number?
-                if day_trades < 4:
-                
-                    # Set "up" status to 2 for while loop
-                    up = 2
-
+                if check_dt():
                     # Buy $20 DOGE
                     print(rh.order_buy_crypto_by_price('DOGE' , 20))
-
-                    # Get current DOGE value at purchase
-                    buy_price = rh.get_crypto_quote('DOGE')['ask_price']
-
-                    # Wait for order to process
-                    time.sleep(60)
-
-                    # Get current DOGE value 60 seconds after purchase
-                    old_price = rh.get_crypto_quote('DOGE')['ask_price']
-
-                    # Loop to check price every minute
-                    # Sell if price falls for two mins in a row
-                    # If "up" drops to 0, exit loop
-                    while up > 0:
-                        time.sleep(60)
-                        new_price = rh.get_crypto_quote('DOGE')['ask_price']
-                        if new_price > old_price:
-                            old_price = new_price
-                            new_price = rh.get_crypto_quote('DOGE')['ask_price']
-                            up = 2
-                        else:
-                            old_price = new_price
-                            new_price = rh.get_crypto_quote('DOGE')['ask_price']
-                            up -= 1
-
-                        # Calculate amount to sell
-                        sell_amount = ((20*current_price)/buy_price)
-
-                        # Sell new amount of dogecoin
-                        print(rh.order_sell_crypto_by_price('DOGE', sell_amount))
-
+                    stop_loss()
             return True
-
         return True
-
 
     def on_error(self, status_code):
         if status_code == 420:
